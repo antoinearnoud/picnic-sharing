b4ece46840d19e0c8a91a3970f4c266b134d7a3a
hugothom <hugothom.salamanca@gmail.com>
2015-08-14 22:19:50 +0200

    mise a jour avec l'ancienne version

--- picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
+++ picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
@@ -1,38 +1,26 @@
 package hugosalamanca.picnic;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-// Antoine comment test
 
-public class MainActivity extends ActionBarActivity {
+public class Listedecourse extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listedecourse);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_listedecourse, menu);
         return true;
     }
-
-    public void listedecourse(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, Listedecourse.class);
-        startActivity(intent);
-    }
-
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
