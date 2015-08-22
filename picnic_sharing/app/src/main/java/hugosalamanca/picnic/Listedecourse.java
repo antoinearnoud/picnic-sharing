cd49eb0bb9b64cad60dd5067d3a5fdc4a5475913
hugothom <hugothom.salamanca@gmail.com>
2015-08-22 18:41:37 +0200

    Début de la mise en forme de la liste de courses

--- picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
+++ picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
@@ -1,29 +1,29 @@
 package hugosalamanca.picnic;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.ListActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 
 public class Listedecourse extends ActionBarActivity {
+    ExpandableListView list_Boisson;
+    ChaptersListAdapter Boissons;
 
-
-    AutoCompleteTextView autoCompleteTextView;
-    String[] Product_Names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listedecourse);
 
-        // this launch the text fill
-        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.product);
-        Product_Names = getResources().getStringArray(R.array.product_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Product_Names);
-        autoCompleteTextView.setAdapter(adapter);
+
     }
 
     @Override
@@ -31,6 +31,28 @@ public class Listedecourse extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_listedecourse, menu);
         return true;
+    }
+
+    private void setupChaptersListView() {
+        list_Boisson = (ExpandableListView)findViewById(R.id.list_Boisson);
+        Boissons = new ChaptersListAdapter();
+        elvChapters.setAdapter(elaAdapter);
+        elvChapters.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+
+                String exerciseTitle = (String) elaAdapter.getChild(groupPosition, childPosition);
+                Class<? extends Activity> exerciseClass = elaAdapter.getExerciseClass(groupPosition, childPosition, id);
+                if (exerciseClass != null) {
+                    Toast.makeText(DemoSelector.this, exerciseTitle, Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(DemoSelector.this, exerciseClass));
+                } else {
+                    Toast.makeText(DemoSelector.this, "Exercise Not Available", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
