42d9fd1c29858c3b223356ce761029c6ac79f2a8
hugothom <hugothom.salamanca@gmail.com>
2015-08-23 20:00:26 +0200

    Beginning of Layout of liste de course

--- picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
+++ picnic_sharing/app/src/main/java/hugosalamanca/picnic/Listedecourse.java
@@ -1,72 +1,156 @@
 package hugosalamanca.picnic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.app.ListActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
 
 
-public class Listedecourse extends ActionBarActivity {
-    ExpandableListView list_Boisson;
-    ChaptersListAdapter Boissons;
+
+public class Listedecourse extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listedecourse);
 
+        expListView = (ExpandableListView) findViewById(R.id.list_Boissons);
 
-    }
+        // preparing list data
+        prepareListData();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_listedecourse, menu);
-        return true;
-    }
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
-    private void setupChaptersListView() {
-        list_Boisson = (ExpandableListView)findViewById(R.id.list_Boisson);
-        Boissons = new ChaptersListAdapter();
-        elvChapters.setAdapter(elaAdapter);
-        elvChapters.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
-                String exerciseTitle = (String) elaAdapter.getChild(groupPosition, childPosition);
-                Class<? extends Activity> exerciseClass = elaAdapter.getExerciseClass(groupPosition, childPosition, id);
-                if (exerciseClass != null) {
-                    Toast.makeText(DemoSelector.this, exerciseTitle, Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(DemoSelector.this, exerciseClass));
-                } else {
-                    Toast.makeText(DemoSelector.this, "Exercise Not Available", Toast.LENGTH_SHORT).show();
-                }
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
 
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // Adding child data
+        listDataHeader.add("Viande");
+        listDataHeader.add("Alcohol");
+        listDataHeader.add("Boissons");
+        listDataHeader.add("Snacks");
+        listDataHeader.add("Matériel");
+        listDataHeader.add("Divers");
 
-        return super.onOptionsItemSelected(item);
+
+        // Adding child data
+        List<String> Viande = new ArrayList<String>();
+        Viande.add("Jambon blanc");
+        Viande.add("Jambon cru");
+        Viande.add("Saucisson");
+
+
+        List<String> Alcohol = new ArrayList<String>();
+        Alcohol.add("Martini");
+        Alcohol.add("Vin Rouge");
+        Alcohol.add("Vin Blanc");
+
+        List<String> Boissons = new ArrayList<String>();
+        Boissons.add("Jus d'orange");
+        Boissons.add("Jus de pomme");
+        Boissons.add("Jus autre");
+        Boissons.add("Eau");
+
+        List<String> Snacks = new ArrayList<String>();
+        Snacks.add("Pâté");
+        Snacks.add("Tarama");
+        Snacks.add("Tsatsiki");
+        Snacks.add("Chips");
+        Snacks.add("Gâteau apéro");
+
+        List<String> Matériel = new ArrayList<String>();
+        Matériel.add("Nappe");
+        Matériel.add("Ouvre bouteille");
+        Matériel.add("Verres");
+        Matériel.add("Assiettes");
+        Matériel.add("Couverts");
+
+        List<String> Divers = new ArrayList<String>();// pour le moment rien de précis
+        Divers.add("Oublis");
+
+
+        listDataChild.put(listDataHeader.get(0), Viande); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), Alcohol);
+        listDataChild.put(listDataHeader.get(2), Boissons);
+        listDataChild.put(listDataHeader.get(3), Snacks);
+        listDataChild.put(listDataHeader.get(4), Matériel);
+        listDataChild.put(listDataHeader.get(5), Divers);
     }
+
 }
